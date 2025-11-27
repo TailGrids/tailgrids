638fca4bc35884dd7ef9351824085a5b2079a461
Johurul Haque <johurul@pimjo.com>
2025-11-27 18:35:26 +0600

    feat: prevent crawlers from indexing on test deployment

--- apps/docs/src/app/robots.ts
+++ apps/docs/src/app/robots.ts
@@ -1,11 +1,14 @@
-import type { MetadataRoute } from 'next';
+import type { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
-      userAgent: '*',
-      allow: '/',
+      userAgent: "*",
+      disallow: "/"
     },
-    sitemap: new URL('/sitemap.xml', process.env.NEXT_PUBLIC_SITE_URL).href,
+    sitemap:
+      process.env.NEXT_PUBLIC_DEPLOYMENT_ENV === "production"
+        ? new URL("/sitemap.xml", process.env.NEXT_PUBLIC_SITE_URL).href
+        : undefined
   };
 }
