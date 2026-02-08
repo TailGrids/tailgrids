5785c638c1efa99e0592f4f17c099fca8684d308
Johurul Haque <johurul@pimjo.com>
2026-02-08 15:40:38 +0600

    refactor: input with theme variables

--- apps/docs/src/registry/core/input.tsx
+++ apps/docs/src/registry/core/input.tsx
@@ -1,32 +1,37 @@
-import { cn } from '@/utils/cn';
-import { cva, type VariantProps } from 'class-variance-authority';
-import { useId, type ComponentProps } from 'react';
+import { cn } from "@/utils/cn";
+import { cva, type VariantProps } from "class-variance-authority";
+import { useId, type ComponentProps } from "react";
 
 const inputStyles = cva(
-  'bg-neutral peer w-full rounded-lg border px-4 py-2.5 text-neutral-800 placeholder:text-neutral-400 focus:ring-4 disabled:border-neutral-200 disabled:text-neutral-300 disabled:placeholder:text-neutral-300',
+  "bg-input-background peer max-w-full rounded-lg border px-4 py-2.5 text-title-50 placeholder:text-input-placeholder-text focus:ring-4 disabled:border-base-100 disabled:text-input-disabled-text disabled:placeholder:text-input-disabled-text outline-none",
   {
     variants: {
       state: {
         default:
-          'focus:border-primary-300 focus:ring-primary-100 border-neutral-300',
-        error: 'border-danger-outline focus:ring-danger-ring',
-        success: 'border-success-outline focus:ring-success-ring',
-      },
-    },
-  },
+          "focus:border-primary-300 focus:ring-primary-300/20 border-base-300",
+        error:
+          "border-input-error-focus-border focus:ring-input-error-focus-border/20",
+        success:
+          "border-input-success-focus-border focus:ring-input-success-focus-border/20"
+      }
+    }
+  }
 );
 
-const hintStyles = cva('text-sm font-normal peer-disabled:text-neutral-300', {
-  variants: {
-    state: {
-      default: 'text-neutral-700',
-      error: 'text-red-500',
-      success: 'text-green-500',
-    },
-  },
-});
+const hintStyles = cva(
+  "text-sm font-normal peer-disabled:text-input-disabled-text",
+  {
+    variants: {
+      state: {
+        default: "text-text-50",
+        error: "text-input-error",
+        success: "text-input-success"
+      }
+    }
+  }
+);
 
-type PropsType = ComponentProps<'input'> &
+type PropsType = ComponentProps<"input"> &
   VariantProps<typeof inputStyles> & {
     label?: string;
     hint?: string;
@@ -34,7 +39,7 @@ type PropsType = ComponentProps<'input'> &
 
 export function Input({
   label,
-  state = 'default',
+  state = "default",
   hint,
   className,
   ...inputProps
@@ -42,11 +47,11 @@ export function Input({
   const id = useId();
 
   return (
-    <div className="grid grid-cols-1 gap-2">
+    <div className="grid grid-cols-1 gap-2 w-full">
       {label && (
         <label
           htmlFor={id}
-          className="max-w-fit text-sm font-medium text-neutral-700 select-none"
+          className="max-w-fit text-sm font-medium text-input-label-text select-none"
         >
           {label}
         </label>
