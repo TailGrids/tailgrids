db6ac73fb41b87b1ba17e1e5a32c0ce072a3eaf8
tusar-pimjo <tusar.pimjo@gmail.com>
2026-03-14 14:48:10 +0600

    feat(chart): create tooltip component

--- apps/docs/src/components/preview/chart/chart-preview.tsx
+++ apps/docs/src/components/preview/chart/chart-preview.tsx
@@ -1,42 +1,46 @@
 "use client";
 
-import { ChartContainer } from "@/registry/core/chart";
-import { Area, AreaChart, CartesianGrid } from "recharts";
+import {
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent
+} from "@/registry/core/chart";
+import { Area, AreaChart, CartesianGrid, XAxis, YAxis } from "recharts";
 
 export default function ChartPreview() {
   const data = [
     {
-      name: "Page A",
+      name: "January",
       uv: 400,
       pv: 2400,
       amt: 2400
     },
     {
-      name: "Page B",
+      name: "February",
       uv: 300,
       pv: 4567,
       amt: 2400
     },
     {
-      name: "Page C",
+      name: "March",
       uv: 320,
       pv: 1398,
       amt: 2400
     },
     {
-      name: "Page D",
+      name: "April",
       uv: 200,
       pv: 9800,
       amt: 2400
     },
     {
-      name: "Page E",
+      name: "May",
       uv: 278,
       pv: 3908,
       amt: 2400
     },
     {
-      name: "Page F",
+      name: "June",
       uv: 189,
       pv: 4800,
       amt: 2400
@@ -44,8 +48,13 @@ export default function ChartPreview() {
   ];
 
   return (
-    <div className="p-10 bg-background-50 rounded">
-      <ChartContainer>
+    <div className="w-80 pl-0 p-4 md:pl-0 md:p-8 md:w-140 aspect-video bg-background-50 rounded">
+      <ChartContainer
+        initialDimension={{
+          width: 280,
+          height: 160
+        }}
+      >
         <AreaChart data={data}>
           <defs>
             <linearGradient id="background" x1="0" y1="0" x2="0" y2="1">
@@ -60,7 +69,17 @@ export default function ChartPreview() {
             fill="url(#background)"
             type="monotone"
           />
+          <XAxis
+            dataKey="name"
+            axisLine={false}
+            tickLine={false}
+            tickMargin={6}
+            tickCount={6}
+            tickFormatter={value => value.slice(0, 3)}
+          />
+          <YAxis dataKey="uv" axisLine={false} tickLine={false} />
 
+          <ChartTooltip content={<ChartTooltipContent />} />
           <CartesianGrid vertical={false} />
         </AreaChart>
       </ChartContainer>
--- apps/docs/src/registry/core/chart.tsx
+++ apps/docs/src/registry/core/chart.tsx
@@ -1,28 +1,123 @@
 "use client";
 
 import { cn } from "@/utils/cn";
-import * as RechartsPrimitives from "recharts";
+import { cva, VariantProps } from "class-variance-authority";
+import * as RechartsPrimitive from "recharts";
 
 function ChartContainer({
-  width = 480,
-  minHeight = 160,
   className,
   children,
   ...props
-}: RechartsPrimitives.ResponsiveContainerProps) {
+}: RechartsPrimitive.ResponsiveContainerProps) {
   return (
-    <RechartsPrimitives.ResponsiveContainer
-      width={width}
-      minHeight={minHeight}
+    <RechartsPrimitive.ResponsiveContainer
       className={cn(
-        "text-xs -tracking-[0.2px] [&_.recharts-wrapper_*]:focus:not-focus-visible:outline-none [&_.recharts-wrapper_*]:focus-visible:outline-2 [&_.recharts-cartesian-grid_line]:stroke-(--color-chart-grid)",
+        "text-xs -tracking-[0.2px] [&_.recharts-wrapper_*]:focus:not-focus-visible:outline-none [&_.recharts-wrapper_*]:focus-visible:outline-2 [&_.recharts-cartesian-grid_line]:stroke-(--color-chart-grid) [&_.recharts-cartesian-axis-tick-value]:fill-(--color-chart-tick)",
         className
       )}
       {...props}
     >
       {children}
-    </RechartsPrimitives.ResponsiveContainer>
+    </RechartsPrimitive.ResponsiveContainer>
   );
 }
 
-export { ChartContainer };
+const ChartTooltip = RechartsPrimitive.Tooltip;
+
+const chartTooltipVariants = cva(
+  "p-2 pl-5 text-title-50 font-medium relative",
+  {
+    variants: {
+      indicator: {
+        dot: "before:absolute before:left-2 before:top-1/2 before:-translate-y-1/2 before:content-[''] before:size-2 before:rounded-full before:bg-primary-500",
+        line: "before:absolute before:left-2 before:top-1/2 before:-translate-y-1/2 before:content-[''] before:w-1 before:h-2.5 before:rounded-none before:bg-primary-500"
+      },
+      indicatorIndex: {
+        0: "before:bg-primary-500",
+        1: "before:bg-primary-400",
+        2: "before:bg-primary-300",
+        3: "before:bg-primary-200",
+        4: "before:bg-primary-100"
+      }
+    },
+    defaultVariants: {
+      indicator: "dot",
+      indicatorIndex: 0
+    }
+  }
+);
+
+interface ChartTooltipContentProps
+  extends
+    Partial<RechartsPrimitive.TooltipContentProps>,
+    VariantProps<typeof chartTooltipVariants> {
+  hideLabel?: boolean;
+  hideIndicator?: boolean;
+  className?: string;
+}
+
+function ChartTooltipContent({
+  payload,
+  label,
+  labelStyle,
+  labelFormatter,
+  labelClassName,
+  active,
+  formatter,
+  className,
+  hideLabel,
+  hideIndicator,
+  indicator = "line"
+}: ChartTooltipContentProps) {
+  if (!active || !payload?.length) {
+    return null;
+  }
+
+  if (!hideLabel && labelFormatter) {
+    return (
+      <div style={labelStyle} className={cn("font-medium", labelClassName)}>
+        {labelFormatter(label, payload)}
+      </div>
+    );
+  }
+
+  return (
+    <div
+      className={cn("bg-white rounded-md overflow-hidden min-w-20", className)}
+    >
+      {!hideLabel && (
+        <p className="px-2 py-1 border-b bg-background-soft-100">{label}</p>
+      )}
+
+      <ul className="space-y-0.5 last:mb-0">
+        {payload
+          .filter(item => item.type !== "none")
+          .map((item, index) => (
+            <li key={item.key}>
+              {formatter && item?.value !== undefined && item.name ? (
+                formatter(item.value, item.name, item, index, item.payload)
+              ) : (
+                <p
+                  className={cn(
+                    chartTooltipVariants({
+                      indicator: hideIndicator ? null : indicator,
+                      indicatorIndex: hideIndicator
+                        ? null
+                        : (index as 0 | 1 | 2 | 3 | 4)
+                    }),
+                    hideIndicator && "pl-2"
+                  )}
+                >
+                  {item.value}
+                </p>
+              )}
+            </li>
+          ))}
+      </ul>
+    </div>
+  );
+}
+
+const ChartLegend = RechartsPrimitive.Legend;
+
+export { ChartContainer, ChartLegend, ChartTooltip, ChartTooltipContent };
