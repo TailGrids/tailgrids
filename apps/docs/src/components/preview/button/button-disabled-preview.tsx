020977a5994ee0d190b79ea024bb264225e66b51
tusar-pimjo <tusar.pimjo@gmail.com>
2026-06-10 07:13:11 +0600

    chore(button): update button doc

--- apps/docs/src/components/preview/button/button-disabled-preview.tsx
+++ apps/docs/src/components/preview/button/button-disabled-preview.tsx
@@ -4,15 +4,12 @@ import { RefreshCircle1Clockwise } from "@tailgrids/icons";
 export default function ButtonDisabledPreview() {
   return (
     <div className="flex flex-wrap items-center gap-4">
-  <Button disabled>Disabled Button</Button>
+      <Button disabled>Disabled Button</Button>
 
       <Button disabled pending>
         <RefreshCircle1Clockwise className="animate-spin" />
         Saving...
       </Button>
-
     </div>
   );
 }
-
-
